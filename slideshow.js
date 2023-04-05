b749f013ab756e5356eb364aca8cad21782ee97b
lolonoa-zoro <129834927+lolonoa-zoro@users.noreply.github.com>
2023-04-05 18:19:32 +0530

    Add files via upload

---
+++ slideshow.js
@@ -0,0 +1,66 @@
+// Your JavaScript code goes here
+
+// set the slideshow interval to change every 3 seconds
+const interval = 3000;
+
+// get all images in the slideshow
+const images = document.querySelectorAll('.slideshow img');
+
+// set the index of the currently displayed image
+let index = 0;
+
+// show the first image
+images[index].style.display = 'block';
+
+// function to change the displayed image
+function changeImage() {
+  // hide the currently displayed image
+  images[index].style.display = 'none';
+
+  // increment the index
+  index++;
+
+  // if the index is greater than the number of images, reset it to 0
+  if (index >= images.length) {
+    index = 0;
+  }
+
+  // show the next image
+  images[index].style.display = 'block';
+}
+
+// set the slideshow interval to change images every 3 seconds
+setInterval(changeImage, interval);
+
+const slides = document.querySelectorAll('.slide');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+let index = 0;
+
+function showSlide() {
+  slides.forEach((slide) => {
+    slide.style.display = 'none';
+  });
+  slides[index].style.display = 'block';
+}
+
+function nextSlide() {
+  index++;
+  if (index > slides.length - 1) {
+    index = 0;
+  }
+  showSlide();
+}
+
+function prevSlide() {
+  index--;
+  if (index < 0) {
+    index = slides.length - 1;
+  }
+  showSlide();
+}
+
+prev.addEventListener('click', prevSlide);
+next.addEventListener('click', nextSlide);
+
+showSlide();

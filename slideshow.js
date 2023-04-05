b7cc9df5846ab645a593dc35f944cb45aabf61d9
lolonoa-zoro <ask141925@gmail.com>
2023-04-05 19:46:16 +0530

    fix slideshow

--- slideshow.js
+++ slideshow.js
@@ -1,5 +1,3 @@
-// Your JavaScript code goes here
-
 // set the slideshow interval to change every 3 seconds
 const interval = 3000;
 
@@ -32,42 +30,40 @@ function changeImage() {
 // set the slideshow interval to change images every 3 seconds
 setInterval(changeImage, interval);
 
-const slides = document.querySelectorAll('.slide');
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
-let index = 0;
-
-document.addEventListener("DOMContentLoaded", function() {
-  var slides = document.querySelectorAll(".slide");
-  for (var i = 1; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-});
-
-function showSlide() {
-  slides.forEach((slide) => {
-    slide.style.display = 'none';
-  });
-  slides[index].style.display = 'block';
-}
 
 function nextSlide() {
+  // hide the currently displayed image
+  images[index].style.display = 'none';
+
+  // increment the index
   index++;
-  if (index > slides.length - 1) {
+
+  // if the index is greater than the number of images, reset it to 0
+  if (index >= images.length) {
     index = 0;
   }
-  showSlide();
+
+  // show the next image
+  images[index].style.display = 'block';
 }
 
 function prevSlide() {
+  // hide the currently displayed image
+  images[index].style.display = 'none';
+
+  // decrement the index
   index--;
+
+  // if the index is less than 0, set it to the last image index
   if (index < 0) {
-    index = slides.length - 1;
+    index = images.length - 1;
   }
-  showSlide();
+
+  // show the previous image
+  images[index].style.display = 'block';
 }
 
 prev.addEventListener('click', prevSlide);
 next.addEventListener('click', nextSlide);
-
-showSlide();

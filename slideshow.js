7fa35bb50f1b299470ce40f9ae9ec921485a81d3
lolonoa-zoro <ask141925@gmail.com>
2023-04-06 23:10:26 +0530

    update

--- slideshow.js
+++ slideshow.js
@@ -3,7 +3,7 @@ const slides = slider.querySelector('.slider-images');
 const slide = slides.querySelectorAll('.slider-image');
 
 let currentSlide = 0;
-let slideInterval = setInterval(nextSlide, 5000);
+let slideInterval = setInterval(nextSlide, 3000);
 
 function nextSlide() {
   goToSlide(currentSlide + 1);
@@ -14,10 +14,12 @@ function prevSlide() {
 }
 
 function goToSlide(n) {
+  clearInterval(slideInterval);
   slide[currentSlide].classList.remove('active');
   currentSlide = (n + slide.length) % slide.length;
   slide[currentSlide].classList.add('active');
   slides.style.transform = `translateX(-${currentSlide * 100}%)`;
+  slideInterval = setInterval(nextSlide, 3000);
 }
 
 const prev = slider.querySelector('.slider-prev');
@@ -25,16 +27,10 @@ const next = slider.querySelector('.slider-next');
 
 prev.addEventListener('click', () => {
   prevSlide();
-  clearInterval(slideInterval);
-  slideInterval = setInterval(nextSlide, 5000);
 });
 
 next.addEventListener('click', () => {
   nextSlide();
-  clearInterval(slideInterval);
-  slideInterval = setInterval(nextSlide, 5000);
 });
 
-setInterval(() => {
-  nextSlide();
-}, 3000);
+slideInterval = setInterval(nextSlide, 3000);

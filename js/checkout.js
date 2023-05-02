596e5164889eb0f6fcd3388a39f3313835c54e0b
lolonoa-zoro <ask141925@gmail.com>
2023-05-02 20:22:42 +0530

    .

--- js/checkout.js
+++ js/checkout.js
@@ -1,79 +1,62 @@
-// Define the URL of the Acme Products API endpoint
-const apiUrl = "https://api.acmeproducts.com/products/123";
+const products = {
+  'Groceries': {
+    'Groceries1': {'name': 'Product 1', 'brand': 'Brand of Product 1', 'price': 10.00},
+    'Groceries2': {'name': 'Product 2', 'brand': 'Brand of Product 2', 'price': 20.00},
+    'Groceries3': {'name': 'Product 3', 'brand': 'Brand of Product 3', 'price': 30.00},
+    'Groceries4': {'name': 'Product 4', 'brand': 'Brand of Product 4', 'price': 40.00},
+    'Groceries5': {'name': 'Product 5', 'brand': 'Brand of Product 5', 'price': 50.00},
+    'Groceries6': {'name': 'Product 6', 'brand': 'Brand of Product 6', 'price': 60.00},
+    'Groceries7': {'name': 'Product 7', 'brand': 'Brand of Product 7', 'price': 70.00},
+    'Groceries8': {'name': 'Product 8', 'brand': 'Brand of Product 8', 'price': 80.00}
+  },
 
-// Use the fetch() function to retrieve the product information from the API
-fetch(apiUrl)
-  .then(response => response.json()) // Parse the JSON response
-  .then(product => {
-    // Extract the relevant information from the product object
-    const name = product.product_name;
-    const price = product.price;
-    const imageUrl = product.image_url;
-    const description = product.description;
+  'Exhibition_Special': {
+    'Exhibition_Special1': {'name': 'Product 1', 'brand': 'Brand of Product 1', 'price': 10.00},
+    'Exhibition_Special2': {'name': 'Product 2', 'brand': 'Brand of Product 2', 'price': 20.00},
+    'Exhibition_Special3': {'name': 'Product 3', 'brand': 'Brand of Product 3', 'price': 30.00},
+    'Exhibition_Special4': {'name': 'Product 4', 'brand': 'Brand of Product 4', 'price': 40.00},
+    'Exhibition_Special5': {'name': 'Product 5', 'brand': 'Brand of Product 5', 'price': 50.00},
+    'Exhibition_Special6': {'name': 'Product 6', 'brand': 'Brand of Product 6', 'price': 60.00},
+    'Exhibition_Special7': {'name': 'Product 7', 'brand': 'Brand of Product 7', 'price': 70.00},
+    'Exhibition_Special8': {'name': 'Product 8', 'brand': 'Brand of Product 8', 'price': 80.00},
+    'Exhibition_Special9': {'name': 'Product 9', 'brand': 'Brand of Product 9', 'price': 90.00},
+    'Exhibition_Special10': {'name': 'Product 10', 'brand': 'Brand of Product 10', 'price': 100.00},
+    'Exhibition_Special11': {'name': 'Product 11', 'brand': 'Brand of Product 11', 'price': 110.00},
+    'Exhibition_Special12': {'name': 'Product 12', 'brand': 'Brand of Product 12', 'price': 80.00}
+  },
 
-    // Find the container element on the webpage where the product information will be displayed
-    const productContainer = document.querySelector("#product-info");
+  'Mens': {
+    'Mens1': {'name': 'Product 1', 'brand': 'Brand of Product 1', 'price': 10.00},
+    'Mens2': {'name': 'Product 2', 'brand': 'Brand of Product 2', 'price': 20.00},
+    'Mens3': {'name': 'Product 3', 'brand': 'Brand of Product 3', 'price': 30.00},
+    'Mens4': {'name': 'Product 4', 'brand': 'Brand of Product 4', 'price': 40.00},
+    'Mens5': {'name': 'Product 5', 'brand': 'Brand of Product 5', 'price': 50.00},
+    'Mens6': {'name': 'Product 6', 'brand': 'Brand of Product 6', 'price': 60.00},
+    'Mens7': {'name': 'Product 7', 'brand': 'Brand of Product 7', 'price': 70.00},
+    'Mens8': {'name': 'Product 8', 'brand': 'Brand of Product 8', 'price': 80.00}
+  },
 
-    // Create the HTML code to display the product information
-    const productHtml = `
-      <img src="${imageUrl}" alt="${name}">
-      <h2>${name}</h2>
-      <p>Price: ${price}</p>
-      <p>Description: ${description}</p>
-    `;
 
-    // Add the product HTML code to the container element
-    productContainer.innerHTML = productHtml;
-  })
-  .catch(error => {
-    console.error("Error retrieving product information:", error);
-  });
+  'Womens': {
+    'Womens1': {'name': 'Product 1', 'brand': 'Brand of Product 1', 'price': 10.00},
+    'Womens2': {'name': 'Product 2', 'brand': 'Brand of Product 2', 'price': 20.00},
+    'Womens3': {'name': 'Product 3', 'brand': 'Brand of Product 3', 'price': 30.00},
+    'Womens4': {'name': 'Product 4', 'brand': 'Brand of Product 4', 'price': 40.00},
+    'Womens5': {'name': 'Product 5', 'brand': 'Brand of Product 5', 'price': 50.00},
+    'Womens6': {'name': 'Product 6', 'brand': 'Brand of Product 6', 'price': 60.00},
+    'Womens7': {'name': 'Product 7', 'brand': 'Brand of Product 7', 'price': 70.00},
+    'Womens8': {'name': 'Product 8', 'brand': 'Brand of Product 8', 'price': 80.00}
+  },
 
-// Define the form submission event handler
-const handleSubmit = event => {
-  event.preventDefault(); // Prevent the default form submission behavior
-
-  // Extract the user's information from the form
-  const firstName = document.querySelector("#first-name").value;
-  const lastName = document.querySelector("#last-name").value;
-  const email = document.querySelector("#email").value;
-  const address = document.querySelector("#address").value;
-
-  // Create the order object
-  const order = {
-    product: {
-      name: document.querySelector("#product-name").textContent,
-      price: document.querySelector("#product-price").textContent,
-    },
-    customer: {
-      firstName,
-      lastName,
-      email,
-      address,
-    },
-  };
-
-  // Submit the order to the API
-  fetch("https://api.acmeproducts.com/orders", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(order),
-  })
-    .then(response => {
-      if (response.ok) {
-        alert("Your order has been submitted successfully.");
-        document.querySelector("form").reset(); // Reset the form
-      } else {
-        throw new Error("Error submitting order.");
-      }
-    })
-    .catch(error => {
-      console.error("Error submitting order:", error);
-      alert("There was an error submitting your order. Please try again.");
-    });
-};
-
-// Attach the form submission event handler to the form element
-document.querySelector("form").addEventListener("submit", handleSubmit);
+  'Shoes': {
+    'Shoes1': {'name': 'Product 1', 'brand': 'Brand of Product 1', 'price': 10.00},
+    'Shoes2': {'name': 'Product 2', 'brand': 'Brand of Product 2', 'price': 20.00},
+    'Shoes3': {'name': 'Product 3', 'brand': 'Brand of Product 3', 'price': 30.00},
+    'Shoes4': {'name': 'Product 4', 'brand': 'Brand of Product 4', 'price': 40.00},
+    'Shoes5': {'name': 'Product 5', 'brand': 'Brand of Product 5', 'price': 50.00},
+    'Shoes6': {'name': 'Product 6', 'brand': 'Brand of Product 6', 'price': 60.00},
+    'Shoes7': {'name': 'Product 7', 'brand': 'Brand of Product 7', 'price': 70.00},
+    'Shoes8': {'name': 'Product 8', 'brand': 'Brand of Product 8', 'price': 80.00}
+  }
+}
+const product = products.find((p) => p.name === productName);
